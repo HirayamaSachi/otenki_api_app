2c2a09a2779c40cc765528e527e9ce274e0243a4
HirayamaSachi <hirayamas0305@gmail.com>
2024-07-21 20:51:20 +0900

    fix:今日の洗濯指数のデザイン調整

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter_config/flutter_config.dart';
 
 import 'fech.dart';
@@ -8,6 +9,7 @@ import 'laundry.dart';
 import 'clothing.dart';
 
 void main() async {
+  debugPaintSizeEnabled = true;
   WidgetsFlutterBinding.ensureInitialized();
   await FlutterConfig.loadEnvVariables();
   runApp(const MyApp());
@@ -37,7 +39,7 @@ class _MyAppState extends State<MyApp> {
       darkTheme: ThemeData.dark(),
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Fetch Data Example'),
+          title: const Text('服装と洗濯指数'),
         ),
         body: Center(
           child: FutureBuilder<WeatherList>(
@@ -48,29 +50,60 @@ class _MyAppState extends State<MyApp> {
                 var laundry = Laundry.washable(snapshot.data!.weather_list[0]);
                 var clothing = Clothing.factory(snapshot.data!.weather_list[0]);
                 return Container(
-                  margin: const EdgeInsets.symmetric(vertical: 20),
-                  height: 200,
-                  child: ListView(
-                    scrollDirection: Axis.horizontal,
-                    children: <Widget>[
-                      Text('最高気温:${snapshot.data!.weather_list[0].temp_max}'),
-                      Text('最低気温:${snapshot.data!.weather_list[0].temp_min}'),
-                      (laundry.washable)
-                          ? const Icon(Icons.circle_outlined)
-                          : const Icon(Icons.clear_sharp),
-                      Container(
-                          width: 30,
-                          child: Image(
-                              image: AssetImage(
-                                  'assets/images/${clothing.min_temp_image_path}'))),
-                      Container(
-                          width: 30,
-                          child: Image(
-                              image: AssetImage(
-                                  'assets/images/${clothing.max_temp_image_path}'))),
-                    ],
-                  ),
-                );
+                    margin: const EdgeInsets.symmetric(vertical: 20),
+                    height: 200,
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Container(
+                                width: 100,
+                                child: Image(
+                                    image: AssetImage(
+                                        'assets/images/${clothing.min_temp_image_path}'))),
+                            Container(
+                                width: 50,
+                                child: Image(
+                                    image: AssetImage(
+                                        'assets/images/${clothing.max_temp_image_path}'))),
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            (laundry.washable)
+                                ? const Icon(Icons.circle_outlined)
+                                : const Icon(Icons.clear_sharp),
+                            Text('${snapshot.data!.weather_list[0].temp_max}'),
+                            Text('~'),
+                            Text('${snapshot.data!.weather_list[0].temp_min}'),
+                          ],
+                        )
+                      ],
+                    )
+                    // ListView(
+                    //   scrollDirection: Axis.horizontal,
+                    //   children: <Widget>[
+                    //     Container(
+                    //         width: 30,
+                    //         child: Image(
+                    //             image: AssetImage(
+                    //                 'assets/images/${clothing.min_temp_image_path}'))),
+
+                    //     Container(
+                    //         width: 30,
+                    //         child: Image(
+                    //             image: AssetImage(
+                    //                 'assets/images/${clothing.max_temp_image_path}'))),
+                    //     (laundry.washable)
+                    //         ? const Icon(Icons.circle_outlined)
+                    //         : const Icon(Icons.clear_sharp),
+                    //     Text('最高気温:${snapshot.data!.weather_list[0].temp_max}'),
+                    //     Text('最低気温:${snapshot.data!.weather_list[0].temp_min}'),
+                    //   ],
+                    // ),
+                    );
               } else if (snapshot.hasError) {
                 return Text('${snapshot.error}');
               }
