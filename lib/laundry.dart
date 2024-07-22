d6b54624ffa7e1c1378d85849e8b18141dbbbc0d
HirayamaSachi <hirayamas0305@gmail.com>
2024-07-22 22:49:38 +0900

    fix:少しでも雨が降れば洗濯できないとする

--- lib/laundry.dart
+++ lib/laundry.dart
@@ -6,6 +6,9 @@ class Laundry {
   const Laundry({required this.washable});
 
   factory Laundry.washable(Weather weather) {
+    if(weather.rain > 0) {
+      return const Laundry(washable: false);
+    }
     if(weather.temp_max >= 25) {
       return const Laundry( washable: true);
     } else {
